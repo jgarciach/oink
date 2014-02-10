cd164b02914483b830dec1d768ab92becad82839
jgarciach <jgarciach@gmail.com>
2014-02-10 03:39:17 -0500

    Added balance in currency method

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,16 @@
 class Account < ActiveRecord::Base
   attr_accessible :balance
+
   has_many :transactions
+
+  include ActionView::Helpers::NumberHelper
+
+  def balance_in(currency)
+    if currency
+      number_to_currency(self.balance, unit: currency)
+    else
+      read_attribute(:balance)
+    end
+  end
+
 end
