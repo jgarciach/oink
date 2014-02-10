8b4ae469b695d44e11e2af68c269c42b3478aee7
jgarciach <jgarciach@gmail.com>
2014-02-10 02:35:49 -0500

    Update balance when transactions are created, updated and destroyed

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,36 @@
 class Transaction < ActiveRecord::Base
   attr_accessible :amount, :category, :date, :description, :notes, :account_id, :rolling_balance
+
   belongs_to :account
+
+  after_create :update_balance_with_new_transaction_amount
+  around_update :update_balance_with_new_amount
+  after_destroy :remove_destroyed_amount_from_balance
+
+  def update_balance_with_new_transaction_amount
+    self.update_balance(self.amount)
+    self.rolling_balance = self.account.balance
+    self.save
+  end
+
+  def update_balance_with_new_amount
+    amount = self.amount
+    yield
+    diff = self.amount - amount
+    if diff != 0
+      self.update_balance(diff)
+      self.rolling_balance = self.account.balance
+      self.save
+    end
+  end
+
+  def remove_destroyed_amount_from_balance
+    self.update_balance(-self.amount)
+  end
+
+  def update_balance(amount)
+    self.account.balance += amount
+    self.account.save
+  end
+
 end
